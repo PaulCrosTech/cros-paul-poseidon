0b707a5b9fc0bd502bcd21266ecfd5e19846e56f
Paul CROS <paul.cros.tech@gmail.com>
2025-03-18 15:27:10 +0100

    Ajout gestionnaire de log

--- src/main/java/com/nnk/springboot/PoseidonApplication.java
+++ src/main/java/com/nnk/springboot/PoseidonApplication.java
@@ -1,13 +1,16 @@
 package com.nnk.springboot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class PoseidonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PoseidonApplication.class, args);
+		log.info("====> Application Poseidon started <====");
 	}
 
 }
