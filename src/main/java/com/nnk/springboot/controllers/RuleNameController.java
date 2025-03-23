f683ce9dbca19b070d64306f7fa6d146db4869b0
Paul CROS <paul.cros.tech@gmail.com>
2025-03-23 08:32:32 +0100

    Form Rule :  /list

--- src/main/java/com/nnk/springboot/controllers/RuleNameController.java
+++ src/main/java/com/nnk/springboot/controllers/RuleNameController.java
@@ -1,51 +1,86 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.Rule;
+import com.nnk.springboot.dto.CurvePointDto;
+import com.nnk.springboot.dto.RuleDto;
+import com.nnk.springboot.service.IRuleService;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
+/**
+ * The RuleName controller
+ */
 @Controller
 @Slf4j
+@RequestMapping(path = "/ruleName")
 public class RuleNameController {
-    // TODO: Inject RuleName service
 
-    @RequestMapping("/ruleName/list")
-    public String home(Model model) {
-        // TODO: find all RuleName, add to model
-        log.info("====> GET /ruleName/list <====");
+
+    private final IRuleService ruleService;
+
+    /**
+     * Constructor
+     *
+     * @param ruleService the ruleService
+     */
+    public RuleNameController(IRuleService ruleService) {
+        this.ruleService = ruleService;
+    }
+
+    /**
+     * Add generic attributes to the model
+     *
+     * @param model the model
+     */
+    @ModelAttribute
+    private void addAttributes(Model model) {
         model.addAttribute("menuActivated", "ruleName");
+    }
+
+    /**
+     * Display the RuleName list page
+     *
+     * @param model the model
+     * @return the RuleName list page
+     */
+    @RequestMapping("/list")
+    public String home(Model model) {
+        log.info("====> GET /ruleName/list <====");
+
+        List<RuleDto> ruleDtoList = ruleService.findAll();
+        model.addAttribute("ruleDtoList", ruleDtoList);
+
         return "ruleName/list";
     }
 
-    @GetMapping("/ruleName/add")
+    @GetMapping("/add")
     public String addRuleForm(Rule bid) {
         log.info("====> GET /ruleName/add <====");
         return "ruleName/add";
     }
 
-    @PostMapping("/ruleName/validate")
+    @PostMapping("/validate")
     public String validate(@Valid Rule rule, BindingResult result, Model model) {
         // TODO: check data valid and save to db, after saving return RuleName list
         log.info("====> POST /ruleName/validate <====");
         return "ruleName/add";
     }
 
-    @GetMapping("/ruleName/update/{id}")
+    @GetMapping("/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
         // TODO: get RuleName by Id and to model then show to the form
         log.info("====> GET /ruleName/update/{} <====", id);
         return "ruleName/update";
     }
 
-    @PostMapping("/ruleName/update/{id}")
+    @PostMapping("/update/{id}")
     public String updateRuleName(@PathVariable("id") Integer id, @Valid Rule rule,
                                  BindingResult result, Model model) {
         // TODO: check required fields, if valid call service to update RuleName and return RuleName list
@@ -53,7 +88,7 @@ public class RuleNameController {
         return "redirect:/ruleName/list";
     }
 
-    @GetMapping("/ruleName/delete/{id}")
+    @GetMapping("/delete/{id}")
     public String deleteRuleName(@PathVariable("id") Integer id, Model model) {
         // TODO: Find RuleName by Id and delete the RuleName, return to RuleName list
         log.info("====> GET /ruleName/delete/{} <====", id);
