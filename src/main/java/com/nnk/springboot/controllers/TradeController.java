d6afdb329ca51462729d3ca36b1448c48bb1a8f9
Paul CROS <paul.cros.tech@gmail.com>
2025-03-23 07:51:23 +0100

    Form trade : /list

--- src/main/java/com/nnk/springboot/controllers/TradeController.java
+++ src/main/java/com/nnk/springboot/controllers/TradeController.java
@@ -1,50 +1,80 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.Trade;
+import com.nnk.springboot.dto.TradeDto;
+import com.nnk.springboot.service.ITradeService;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @Slf4j
+@RequestMapping(path = "/trade")
 public class TradeController {
-    // TODO: Inject Trade service
 
-    @RequestMapping("/trade/list")
-    public String home(Model model) {
-        // TODO: find all Trade, add to model
-        log.info("====> GET /trade/list <====");
+
+    private final ITradeService tradeService;
+
+    /**
+     * Constructor
+     *
+     * @param tradeService the Trade service
+     */
+    public TradeController(ITradeService tradeService) {
+        this.tradeService = tradeService;
+    }
+
+    /**
+     * Add generic attributes to the model
+     *
+     * @param model the model
+     */
+    @ModelAttribute
+    private void addAttributes(Model model) {
         model.addAttribute("menuActivated", "trade");
+    }
+
+    /**
+     * Display the Trade list page
+     *
+     * @param model the model
+     * @return the Trade list page
+     */
+    @RequestMapping("/list")
+    public String home(Model model) {
+        log.info("====> GET /trade/list <====");
+
+        List<TradeDto> tradeList = tradeService.findAll();
+        model.addAttribute("tradeList", tradeList);
         return "trade/list";
     }
 
-    @GetMapping("/trade/add")
+    @GetMapping("/add")
     public String addUser(Trade bid) {
         log.info("====> GET /trade/add <====");
         return "trade/add";
     }
 
-    @PostMapping("/trade/validate")
+    @PostMapping("/validate")
     public String validate(@Valid Trade trade, BindingResult result, Model model) {
         // TODO: check data valid and save to db, after saving return Trade list
         log.info("====> POST /trade/validate <====");
         return "trade/add";
     }
 
-    @GetMapping("/trade/update/{id}")
+    @GetMapping("/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
         // TODO: get Trade by Id and to model then show to the form
         log.info("====> GET /trade/update/{} <====", id);
         return "trade/update";
     }
 
-    @PostMapping("/trade/update/{id}")
+    @PostMapping("/update/{id}")
     public String updateTrade(@PathVariable("id") Integer id, @Valid Trade trade,
                               BindingResult result, Model model) {
         // TODO: check required fields, if valid call service to update Trade and return Trade list
@@ -52,7 +82,7 @@ public class TradeController {
         return "redirect:/trade/list";
     }
 
-    @GetMapping("/trade/delete/{id}")
+    @GetMapping("/delete/{id}")
     public String deleteTrade(@PathVariable("id") Integer id, Model model) {
         // TODO: Find Trade by Id and delete the Trade, return to Trade list
         log.info("====> GET /trade/delete/{} <====", id);
